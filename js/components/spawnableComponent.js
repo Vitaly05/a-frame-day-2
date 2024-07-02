8afc888645eacc18cba6a145aa6004577db1a32f
Vitaly05 <vitalik050405@gmail.com>
2024-07-02 21:06:41 +0300

    Added smoothing to jenkins move animation

--- js/components/spawnableComponent.js
+++ js/components/spawnableComponent.js
@@ -1,19 +1,45 @@
 AFRAME.registerComponent('spawnable', {
   schema: {
     modelId: { type: 'string' },
+    duration: { type: 'number', default: 1000 },
   },
   init: function () {
     this.modelEl = document.getElementById(this.data.modelId)
+    this.targetPosition = new THREE.Vector3()
+    this.startPosition = new THREE.Vector3()
+    this.isMoving = false
   },
   events: {
     click: function (e) {
       const intersection = e.detail.intersection
       if (intersection && this.modelEl) {
-        const position = intersection.point
-        this.modelEl.setAttribute('position', { ...position })
         this.modelEl.setAttribute('visible', true)
+        this.targetPosition.copy(intersection.point)
+        this.startPosition.copy(this.modelEl.object3D.position)
+        this.isMoving = true
+        this.startTime = null
       }
     },
+  },
+  tick: function (time, timeDelta) {
+    if (!this.isMoving) {
+      return
+    }
+    if (!this.startTime) {
+      this.startTime = time
+    }
+    const progress = (time - this.startTime) / this.data.duration
+
+    this.modelEl.object3D.position.lerpVectors(
+      this.startPosition,
+      this.targetPosition,
+      progress
+    )
+
+    if (progress >= 1) {
+      this.isMoving = false
+      this.modelEl.setAttribute('visible', true)
+    }
   },
   remove: function () {},
 })
